71ac09e606ad2e10520b90945ee9b78d907ba549
nas.housni <nas.housni@hotmail.fr>
2021-01-26 14:47:10 +0100

    Change Login View

--- src/main/java/com/pharmacy/app/Login.java
+++ src/main/java/com/pharmacy/app/Login.java
@@ -1,21 +1,24 @@
-/*
 package com.pharmacy.app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import org.checkerframework.checker.units.qual.A;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Login extends AnchorPane implements Initializable {
+public class Login extends AnchorPane {
 
 
     private final Label userNameLabel;
@@ -23,6 +26,7 @@ public class Login extends AnchorPane implements Initializable {
 
     private final Label passWordLabel;
     private final PasswordField passwordField;
+
     private final Button login;
 
     private final Label errorMessage;
@@ -30,67 +34,105 @@ public class Login extends AnchorPane implements Initializable {
     private Main application;
 
     public Login() {
-        userNameLabel = new Label();
+        userNameLabel = new Label("Username");
+        userNameLabel.setLayoutX(14.0);
+        userNameLabel.setLayoutY(67.0);
+
         userId = new TextField();
-        passWordLabel = new Label();
+        userId.setPromptText("Username");
+        userId.setLayoutY(86.0);
+        userId.setPrefWidth(415.0);
+
+        passWordLabel = new Label("Password");
+        passWordLabel.setLayoutX(14.0);
+        passWordLabel.setLayoutY(139.0);
+
         passwordField = new PasswordField();
-        login = new Button();
-        errorMessage = new Label();
+        passwordField.setPromptText("Password");
+        passwordField.setLayoutY(157.0);
+        passwordField.setPrefWidth(415.0);
 
-
+        login = new Button("Login");
+        login.setPrefHeight(70.0);
+        login.setPrefWidth(400.0);
+        login.setDefaultButton(true);
         login.setOnAction(this::processLogin);
+
+        errorMessage = new Label("");
+
         AnchorPane anchorPane2 = new AnchorPane();
         anchorPane2.setPrefHeight(300.0);
         anchorPane2.setPrefWidth(500.0);
 
         VBox vBox = new VBox();
+        vBox.setPrefHeight(300.0);
+        vBox.setPrefWidth(421.0);
+        vBox.setSpacing(5.0);
+        vBox.setAlignment(Pos.CENTER);
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setPrefHeight(25.0);
+        hBox.setPrefWidth(418.0);
+        hBox.getChildren().add(errorMessage);
+
+        Image image = new Image(getClass().getResourceAsStream("/images/LoginLogo.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setLayoutY(-12.0);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
 
 
         AnchorPane anchorPane = new AnchorPane();
         anchorPane.setPrefHeight(180.0);
         anchorPane.setPrefWidth(430.0);
-
         anchorPane.getChildren().add(userNameLabel);
         anchorPane.getChildren().add(userId);
         anchorPane.getChildren().add(passWordLabel);
         anchorPane.getChildren().add(passwordField);
+        anchorPane.getChildren().add(imageView);
+
+        AnchorPane.setBottomAnchor(anchorPane2, 200.0);
+        AnchorPane.setLeftAnchor(anchorPane2, 0.0);
+        AnchorPane.setRightAnchor(anchorPane2, 0.0);
+        AnchorPane.setTopAnchor(anchorPane2,0.0);
+
+        AnchorPane.setBottomAnchor(vBox, 0.0);
+        AnchorPane.setLeftAnchor(vBox, 29.0);
+        AnchorPane.setRightAnchor(vBox, 40.0);
+        AnchorPane.setTopAnchor(vBox,0.0);
+
+        AnchorPane.setLeftAnchor(userId,10.0);
+        AnchorPane.setRightAnchor(userId, 0.0);
+
+        AnchorPane.setLeftAnchor(passwordField,10.0);
+        AnchorPane.setRightAnchor(passwordField, 0.0);
+
+        AnchorPane.setBottomAnchor(login, 66.0);
+        AnchorPane.setLeftAnchor(login, 40.0);
+        AnchorPane.setRightAnchor(login, 40.0);
+
+        AnchorPane.setBottomAnchor(hBox, 156.0);
+        AnchorPane.setLeftAnchor(hBox, 41.0);
+        AnchorPane.setRightAnchor(hBox, 41.0);
 
         vBox.getChildren().add(anchorPane);
 
-        ImageView imageView = new ImageView();
-
-        HBox hBox = new HBox();
-        hBox.getChildren().add(errorMessage);
 
         anchorPane2.getChildren().add(vBox);
+        anchorPane2.getStyleClass().add("top-segment");
 
+
+        setPrefHeight(500);
+        setPrefWidth(500);
         getChildren().add(anchorPane2);
         getChildren().add(login);
         getChildren().add(hBox);
+        getStyleClass().add("background");
     }
 
-    public void setApp(Main application){
-        this.application = application;
+    private void processLogin(ActionEvent event) {
     }
 
-    public void processLogin(ActionEvent event) {
-        if (application == null){
-            // We are running in isolated FXML, possibly in Scene Builder.
-            // NO-OP.
-            errorMessage.setText("Hello " + userId.getText());
-        } else {
-            if (!application.userLogging(userId.getText(), passwordField.getText())){
-                errorMessage.setText("Username/Password is incorrect");
-            }
-        }
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        errorMessage.setText("");
-        userId.setPromptText("Username");
-        passwordField.setPromptText("Password");
-    }
 }
 
-*/
--- src/main/java/com/pharmacy/app/Main.java
+++ src/main/java/com/pharmacy/app/Main.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 
 public class Main {
 
-	public static void main(String[] args) {
-  Application.launch(PharmacyApp.class, args);
-	}
+    public static void main(String[] args) { Application.launch(PharmacyApp.class, args); }
 
 }
